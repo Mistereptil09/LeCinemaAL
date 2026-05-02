0a1635c214a079483dae3f91386eb5b9196b1e33
antonio <antoniociorba@outlook.com>
2026-05-02 19:53:42 +0200

    fix: use node24 for the ci. fix: use authFinder for the user class.

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,21 +1,23 @@
-import { UserSchema } from '#database/schema'
-import { hasMany, beforeSave } from '@adonisjs/lucid/orm'
+import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
+import { compose } from '@adonisjs/core/helpers'
+import hash from '@adonisjs/core/services/hash'
+import { BaseModel, hasMany } from '@adonisjs/lucid/orm'
 import type { HasMany } from '@adonisjs/lucid/types/relations'
 import Ticket from '#models/ticket'
 import Transaction from '#models/transaction'
-import hash from '@adonisjs/core/services/hash'
 
-export default class User extends UserSchema {
+const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
+  uids: ['email'],
+  passwordColumnName: 'password',
+})
+
+export default class User extends compose(BaseModel, AuthFinder) {
+  static accessTokens = DbAccessTokensProvider.forModel(User)
+
   @hasMany(() => Ticket)
   declare tickets: HasMany<typeof Ticket>
 
   @hasMany(() => Transaction)
   declare transactions: HasMany<typeof Transaction>
-
-  @beforeSave()
-  static async hashPassword(user: User) {
-    if (user.$dirty.password) {
-      user.password = await hash.make(user.password)
-    }
-  }
 }
