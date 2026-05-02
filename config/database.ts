17fc159468235c785aa976ffed2d138a7d7f8089
antonio <antoniociorba@outlook.com>
2026-05-02 18:34:58 +0200

    commented sqlite connection

--- config/database.ts
+++ config/database.ts
@@ -10,46 +10,6 @@ const dbConfig = defineConfig({
 
   connections: {
     /**
-     * SQLite connection (default).
-     */
-    sqlite: {
-      client: 'better-sqlite3',
-
-      connection: {
-        filename: app.tmpPath('db.sqlite3'),
-      },
-
-      /**
-       * Required by Knex for SQLite defaults.
-       */
-      useNullAsDefault: true,
-
-      migrations: {
-        /**
-         * Sort migration files naturally by filename.
-         */
-        naturalSort: true,
-
-        /**
-         * Paths containing migration files.
-         */
-        paths: ['database/migrations'],
-      },
-
-      schemaGeneration: {
-        /**
-         * Enable schema generation from Lucid models.
-         */
-        enabled: true,
-
-        /**
-         * Custom schema rules file paths.
-         */
-        rulesPaths: ['./database/schema_rules.js'],
-      },
-    },
-
-    /**
      * PostgreSQL connection.
      * Install package to switch: npm install pg
      */
@@ -68,6 +28,46 @@ const dbConfig = defineConfig({
       },
       debug: app.inDev,
     },
+
+    // /**
+    //  * SQLite connection (default).
+    //  */
+    // sqlite: {
+    //   client: 'better-sqlite3',
+    //
+    //   connection: {
+    //     filename: app.tmpPath('db.sqlite3'),
+    //   },
+    //
+    //   /**
+    //    * Required by Knex for SQLite defaults.
+    //    */
+    //   useNullAsDefault: true,
+    //
+    //   migrations: {
+    //     /**
+    //      * Sort migration files naturally by filename.
+    //      */
+    //     naturalSort: true,
+    //
+    //     /**
+    //      * Paths containing migration files.
+    //      */
+    //     paths: ['database/migrations'],
+    //   },
+    //
+    //   schemaGeneration: {
+    //     /**
+    //      * Enable schema generation from Lucid models.
+    //      */
+    //     enabled: true,
+    //
+    //     /**
+    //      * Custom schema rules file paths.
+    //      */
+    //     rulesPaths: ['./database/schema_rules.js'],
+    //   },
+    // },
 
     /**
      * MySQL / MariaDB connection.
